eb3b78b07adb4dbfa22b67eb7269ee0c2c62d4b0
= <mark.pawlowski@webagesolutions.com>
2023-04-25 13:48:30 +0200

    fix code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   //templateUrl: './app.component.html',
   template: `
   <h1>myapp-ang-001</h1>
-  `
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
